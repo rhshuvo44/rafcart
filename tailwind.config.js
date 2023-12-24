d938c06937d1c6285c750e7aca44d70b6931465a
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2023-12-24 17:06:05 +0600

    navbar

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{html,js}",
+    "./pages/**/*.{html,js}",
+    "./index.html",
+  ],
+  theme: {
+    screens: {
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    extend: {
+      fontFamily: {
+        Roboto: "'Roboto', sans-serif",
+        Poppins: "'Poppins', sans-serif",
+      },
+      colors: {
+        primary: "#FD3D57",
+      },
+    },
+  },
+  plugins: [],
+};
